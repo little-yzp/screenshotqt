558b244d6ab747bef1fd1d54d3bbd8df8846956c
yizhuping <2841413637@qq.com>
2025-03-24 16:04:11 +0800

    解决截图后关闭最后一个固定截图主程序结束问题

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
+    //解决bug截图后删除最后一个固定截图程序推出问题
+    a.setQuitOnLastWindowClosed(false);
 #ifdef APP
     ShortcutFilter filter;
     // 注册Ctrl+Alt+S作为快捷键
     const quint32 SHORTCUT_ID = 1;
-    const quint32 MODIFIERS = MOD_CONTROL | MOD_ALT;
+    const quint32 MODIFIERS = MOD_CONTROL | MOD_SHIFT;
     const quint32 KEY = 'S';
     if (!filter.registerShortcut(SHORTCUT_ID, MODIFIERS, KEY)) {
         qDebug("Failed to register hotkey.");
